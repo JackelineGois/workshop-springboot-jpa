29d7670b89e3f2989b5815947cb1d45ea88cd687
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-02-21 13:28:34 +0000

    User Entity and resource

---
+++ src/main/java/com/project/demo/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.project.demo.resources;
+
+import com.project.demo.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+  @GetMapping
+  public ResponseEntity<User> findAll() {
+    User u = new User(1L, "JAcke", "jacke@gmail.com", "999988", "kcsdjio23");
+    return ResponseEntity.ok().body(u);
+  }
+}
