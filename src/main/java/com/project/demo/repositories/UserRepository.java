8ce8f590d886b15febf3364a2612618c35377c8a
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-02-22 19:55:09 +0000

    Service layer, component registration

--- src/main/java/com/project/demo/repositories/UserRepository.java
+++ src/main/java/com/project/demo/repositories/UserRepository.java
@@ -3,4 +3,5 @@ package com.project.demo.repositories;
 import com.project.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {}
